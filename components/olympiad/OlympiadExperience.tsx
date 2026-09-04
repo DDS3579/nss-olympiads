f40cbf5c16ab4bc8b50a1d26a1a3d2c206a50a55
Divya Darsheel Sharma <divyadarsheel.dds@gmail.com>
2026-09-05 00:51:38 +0545

    fix: olympiad experience

---
+++ components/olympiad/OlympiadExperience.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { getOlympiadBySlug } from "@/lib/data/olympiads";
+import { ProgressProvider } from "./context";
+import { OlympiadHero } from "./OlympiadHero";
+import { OlympiadNav } from "./OlympiadNav";
+import { OlympiadOverview } from "./OlympiadOverview";
+import { Constellation } from "./Constellation";
+import { OlympiadTopics } from "./OlympiadTopics";
+import { FeaturedProblem } from "./FeaturedProblem";
+import { StudyMaterialSection } from "./StudyMaterialSection";
+import { ModelPapersSection } from "./ModelPapersSection";
+import { PreparationRoadmap } from "./PreparationRoadmap";
+import { NextStep } from "./NextStep";
+import { OlympiadCTA } from "./OlympiadCTA";
+
+export function OlympiadExperience({ slug }: { slug: string }) {
+  const olympiad = getOlympiadBySlug(slug);
+  if (!olympiad) return null;
+
+  return (
+    <ProgressProvider slug={slug} topics={olympiad.topics}>
+      <div className="relative bg-background text-foreground">
+        <OlympiadHero olympiad={olympiad} />
+        <OlympiadNav />
+        <main className="relative">
+          <OlympiadOverview olympiad={olympiad} />
+          <Constellation olympiad={olympiad} />
+          <OlympiadTopics olympiad={olympiad} />
+          <FeaturedProblem olympiad={olympiad} />
+          <StudyMaterialSection olympiad={olympiad} />
+          <ModelPapersSection olympiad={olympiad} />
+          <PreparationRoadmap olympiad={olympiad} />
+          <OlympiadCTA olympiad={olympiad} />
+        </main>
+        <NextStep olympiad={olympiad} />
+      </div>
+    </ProgressProvider>
+  );
+}
